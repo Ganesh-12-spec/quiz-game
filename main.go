e3b5d8dfe12d3392ca8d4705013e5d1b3311652d
Ganesh Mhetre <mhetreg458@gmail.com>
2026-04-14 05:50:48 +0000

    feat: normalize answers using TrimSpace and ToLower

--- main.go
+++ main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Quiz App Started 🚀")
+	
+	file,err := os.Open("problems.csv")
+	if err != nil{
+		fmt.Println("Erro opening file")
+		return
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+
+	records,err := reader.ReadAll()
+	if err != nil{
+		fmt.Println("Error reading CSV")
+		return
+	}
+	score := 0
+
+	for _, record := range records{
+		var userAnswer string
+
+		fmt.Println(record[0] + "= ?")
+		fmt.Scanln(&userAnswer)
+
+		if strings.TrimSpace(userAnswer) == strings.TrimSpace(record[1]){
+			score++
+		}
+	}
+	fmt.Println("your score:" , score, "/", len(records))
 }
